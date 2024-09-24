fda2b7fb729e34c6d5eafddadb5935e82048bd1b
OtavioAGO <otavioago@outlook.com>
2024-09-24 05:54:38 -0300

    Finalizada parte de cadastro de usuarios

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\otavi\\IdeaProjects\\SistemaCadastros\\src\\formulario.txt"))){
+        Scanner sc = new Scanner(System.in);
+        int opc = sc.nextInt();
+        try(BufferedReader br = new BufferedReader(new FileReader("src\\formularios\\formulario.txt"))){
             String line;
             while ((line = br.readLine()) != null){
                 System.out.println(line);
+            }
+            String nome = sc.nextLine();
+            String email = sc.nextLine();
+            int idade = sc.nextInt();
+            double altura = sc.nextDouble();
+            Pessoa pessoa = new Pessoa(nome, email, idade, altura);
+            int n = 1;
+            String nomeArquivo = n+"-"+pessoa.getNome().toUpperCase()+".txt";
+            File arquivo = new File("src\\formularios\\"+nomeArquivo);
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(arquivo))){
+                while (arquivo.exists()){
+                    nomeArquivo = ++n+"-"+pessoa.getNome().toUpperCase()+".txt";
+                    arquivo = new File("src\\formularios\\"+nomeArquivo);
+                }
+                bw.write(pessoa.toString());
+                bw.flush();
+            }catch (IOException e){
+                e.printStackTrace();
             }
         }catch (IOException e){
             e.printStackTrace();
